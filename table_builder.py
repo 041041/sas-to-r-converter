0668a142ba998aa5fbcec578a0fe5694e715ac99
041041 <bhogal.san@gmail.com>
2026-04-26 15:12:23 +0530

    Update table_builder.py

--- table_builder.py
+++ table_builder.py
@@ -541,7 +541,7 @@ def render_table_builder_tab():
         with r3b:
             add_n_row = st.checkbox("🔢 Add N Row at Top", value=False)
         with r3c:
-            pct_only  = selections.get("pct_only", False)
+            pct_only = st.checkbox("% Only (no counts)", value=False)
             
         selections = {
             "table_type":  table_type,
